3a3661c7aedebc481b4179bcea326c73c5444192
Evan LaBate <49501559+JonGrayson@users.noreply.github.com>
2021-03-29 13:10:49 -0700

    Add files via upload

--- Scripts/Door.cs
+++ Scripts/Door.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Door : MonoBehaviour
 {
     public bool isClosed = false;
     public bool isLocked = false;
+
+    public Toggle locked;
+    public Toggle closed;
 
     Vector3 closedRotation = new Vector3(0, 0, 0);
     Vector3 openRotation = new Vector3(0, -135, 0);
@@ -21,6 +25,12 @@ public class Door : MonoBehaviour
         {
             transform.eulerAngles = openRotation;
         }
+    }
+
+    public void Update()
+    {
+        isDoorLocked();
+        isDoorClosed();
     }
 
     public bool Open()
@@ -44,5 +54,37 @@ public class Door : MonoBehaviour
         }
 
         return true;
+    }
+
+    public bool isDoorLocked()
+    {
+        if (locked.isOn == true)
+        {
+            isLocked = true;
+            return true;
+        }
+
+        else
+        {
+            isLocked = false;
+            return false;
+        }
+    }
+
+    public bool isDoorClosed()
+    {
+        if (closed.isOn == true)
+        {
+            isClosed = true;
+            Close();
+            return true;
+        }
+
+        else
+        {
+            isClosed = false;
+            Open();
+            return false;
+        }
     }
 }
